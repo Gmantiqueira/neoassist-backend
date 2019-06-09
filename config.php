7fe2aa5352c158ef5ffdad9c02df998c92c1d6e4
Gmantiqueira <gmantiqueira@outlook.com>
2019-06-09 13:49:36 -0300

    bugfix | Keywords

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 $greatKeyWords = ['elogi', 'maravil', 'simpat', 'agradav', 'agradec', 'otimo', 'sensacional'];
 $goodKeyWords = ['obrigad', 'parabens'];
 $attentionKeyWords = ['porem', 'so q', 'reclama', 'garantia', 'ainda', 'errad', 'nao', 'ate agora', 'prazo', 'saber', '\?', 'foi ontem', 'cancel', 'verificar'];
-$criticalKeyWords =['nao recebemos','aguardo', 'provid', 'solucao', 'resolv'];
+$criticalKeyWords =['nao recebemos','aguardo', 'provid', 'solucao', 'resolv', 'nao che', 'nao fu'];
 
 $ticketsByPage = 4;
 
